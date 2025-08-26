5a1e8cd54fa228b919053eaed7fefea1601168e2
Mitra Roshanfekr <mitraroshanfekr05@gmail.com>
2025-08-27 01:02:11 +0330

    dynamic sidebar options

--- src/layouts/NavContainer.tsx
+++ src/layouts/NavContainer.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const NavContainer = () => {
   return (
-    <div className='bg-white'>NavContainer</div>
+    <div className='bg-white p-8'>
+      <h1></h1>
+    </div>
   )
 }
 
--- src/layouts/SideBar.tsx
+++ src/layouts/SideBar.tsx
@@ -1,18 +1,21 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
 
 const SideBar = () => {
+  const [activeOptionIdx, setActiveOptionIdx]= useState(0)
+  const [activeSubOptionIdx, setActiveSubOptionIdx] = useState(-1)
 
   const { t } = useTranslation();
 
   const menu = [
     {
       path: "",
-      iconPath: `/assets/icons/active/element-4.svg`,
+      iconPath: `/assets/icons/${activeOptionIdx===0? 'active': 'inactive'}/element-4.svg`,
       option: t("overview"),
     },
     {
       path: "",
-      iconPath: `/assets/icons/active/book.svg`,
+      iconPath: `/assets/icons/${activeOptionIdx===1? 'active': 'inactive'}/book.svg`,
       option: t("courses"),
       dropdown: [
         {
@@ -27,92 +30,52 @@ const SideBar = () => {
     },
     {
       path: "",
-      iconPath: `/assets/icons/active/user-octagon.svg`,
+      iconPath: `/assets/icons/${activeOptionIdx===2? 'active': 'inactive'}/user-octagon.svg`,
       option: t("mentors"),
     },
     {
       path: "",
-      iconPath: `/assets/icons/active/message.svg`,
+      iconPath: `/assets/icons/${activeOptionIdx===3? 'active': 'inactive'}/message.svg`,
       option: t("message"),
     },
     {
       path: "",
-      iconPath: `/assets/icons/active/setting-4.svg`,
+      iconPath: `/assets/icons/${activeOptionIdx===4? 'active': 'inactive'}/setting-4.svg`,
       option: t("setting"),
     },
   ];
-  // const SideBar = () => {
-  //   const menu = [
-  //     {
-  //       path: "",
-  //       iconPath: `/assets/icons/{optionActive? 'active': 'inactive'}/element-4.svg`,
-  //       option: "Overview",
-  //
-  //     },
-  //     {
-  //       path: "",
-  //       iconPath: `/assets/icons/{optionActive? 'active': 'inactive'}/book.svg`,
-  //       option: "Courses",
-  //       dropdown: [
-  //         {
-  //           option: "Explore Courses",
-  //           path: "",
-  //         },
-  //         {
-  //           option: "My Cources",
-  //           path: "",
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       path: "",
-  //       iconPath: `/assets/icons/{optionActive? 'active': 'inactive'}/user-octagon.svg`,
-  //       option: "Mentors",
-  //
-  //     },
-  //     {
-  //       path: "",
-  //       iconPath: `/assets/icons/{optionActive? 'active': 'inactive'}/message.svg`,
-  //       option: "Message",
-  //
-  //     },
-  //     {
-  //       path: "",
-  //       iconPath: `/assets/icons/{optionActive? 'active': 'inactive'}/setting-4.svg`,
-  //       option: "Setting",
-  //
-  //     },
-  //   ];
 
   return (
-    <div className="w-[256px] display-flex flex-col align-middle bg-white p-8 h-screen gap-20">
+    <div className="w-[256px] display-flex flex-col align-middle bg-white p-8 h-screen gap-20 fixed z-10 top-0 left-0 md:relative ">
       <div className="flex gap justify-center">
         <img src="/assets/images/Union.svg" alt="" />
         <img src="/assets/images/Coursea.svg" alt="" />
       </div>
       <div className="display-flex flex-col gap-2 flex-1 ">
         {menu.map((option,i) => (
-          <div key={i} className="gap-4 display-flex flex-col  bg-white">
+          <div key={i} className={`gap-4 display-flex flex-col rounded-[10px] transition-all duration-300  ${activeOptionIdx===i?'bg-[#F5F5F7]':'bg-white'}`} onClick={()=>{activeOptionIdx!==i? setActiveOptionIdx(i):null}}>
             <div className="menu-option border-radius flex space-between ">
               <div className="flex gap ">
                 <img src={option.iconPath} alt="" />
-                <p>{option.option}</p>
+                <p className={`transition-all duration-300 ${activeOptionIdx===i?'text-[#141522]':'text-[#9C9CA4]'}`}>{option.option}</p>
               </div>
               {option.dropdown ? (
-                <img className="" src="/assets/icons/arrow-up.svg" alt="" />
+                <img className={`transition-all duration-300 ${activeOptionIdx===i?'':'rotate-180'}`} src="/assets/icons/arrow-up.svg" alt="" />
               ) : null}
             </div>
-            {option.dropdown
+            <div className={`overflow-hidden transition-all duration-300 ${activeOptionIdx===i?'max-h-40':'max-h-0'}`}>
+              {option.dropdown
               ? option.dropdown.map((option, j) => (
-                  <div key={j} className="gap-2 display-flex flex-col">
+                  <div key={j} className="gap-2 display-flex flex-col" onClick={()=>{activeSubOptionIdx!==j? setActiveSubOptionIdx(j):null}}>
                     <div className="menu-option border-radius flex space-between">
                       <div className="flex gap">
-                        <p>{option.option}</p>
+                        <p className={`transition-all duration-300 ${activeSubOptionIdx===j?'text-[#141522]': 'text-[#9C9CA4]'}`}>{option.option}</p>
                       </div>
                     </div>
                   </div>
                 ))
               : null}
+            </div>
           </div>
         ))}
       </div>
