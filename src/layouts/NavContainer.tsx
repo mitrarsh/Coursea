65697115baed563cd1a9866600a14ee35e9d5281
Mitra Roshanfekr <mitraroshanfekr05@gmail.com>
2025-08-28 23:50:02 +0330

    navbar-started

--- src/layouts/NavContainer.tsx
+++ src/layouts/NavContainer.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const NavContainer = () => {
   return (
-    <div className='bg-white p-8'>
-      <h1></h1>
+    <div className='bg-white p-8 '>
+      <div className="search">
+        <img src="assets/icons/search-normal.svg" alt="" />
+      </div>
+      <div className="notif">
+        <img src="assets/icons/notif.svg" alt="" />
+      </div>
+      <div className="profilePic w-[5rem] h-[5rem] rounded-full overflow-hidden flex justify-center">
+        <img src="assets/images/prof.avif" alt="" />
+      </div>
     </div>
   )
 }
